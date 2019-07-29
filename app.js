49936ede50f204a8f000426895738dd5d7a61d99
Andrew McMechan <coderwurst@gmail.com>
2019-07-29 17:08:04 +0200

    first responses

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express');
 
 const app = express();
 
-// routing app.method.url
+// routing app.http-method.url
 app.get('/', (request, response) => {        // root
-    response.status(200).send('hello from the express server');
-});           
+    response
+    .status(200)
+    .json(
+        {   message: 'hello from the express server',
+            app: 'natours-project'
+        });
+});
 
+app.post('/', (request, response) => {        // root
+    response.status(200).send('post to this end point');
+});
 
 const port = 3000;
 app.listen(port, () => {
