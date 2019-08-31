cbacd6ab7f4ac158552d72f4e6211cb3ff37d8b3
Andrew McMechan <coderwurst@gmail.com>
2019-08-31 20:09:33 +0200

    mongoose to connect to atlas

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful'));
 
 // config file counts for all further files
 const app = require('./app');
-
-console.log(process.env);
 
 const port = process.env.PORT;
 app.listen(port, () => {
