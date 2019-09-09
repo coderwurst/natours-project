fd23b43765d24d0365e16b19e2097e336f77ff54
Andrew McMechan <coderwurst@gmail.com>
2019-09-09 17:17:55 +0200

    debugging for Multiple Text Records not allowed

--- server.js
+++ server.js
@@ -14,10 +14,25 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false
   })
-  .then(() => console.log('DB connection successful'));
+  .then(() => {
+    console.log('DB connection successful');
+  });
+/*.catch(error => {
+    console.log(`Connection failure: ${error}`);
+  });*/
 
 // config file counts for all further files
 const app = require('./app');
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+process.on('warning', warning => {
+  console.warn(warning.name);
+  console.warn(warning.message);
+  console.warn(warning.stack);
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {
