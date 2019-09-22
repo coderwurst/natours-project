885b7c20f89a48f12c5e9d712c084e0bdadb8568
Andrew McMechan <coderwurst@gmail.com>
2019-09-22 20:05:06 +0200

    catch invalid route errors

--- app.js
+++ app.js
@@ -14,12 +14,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json()); // middleware to add data to request body
 app.use(express.static(`${__dirname}/public`)); // serving static files
 
-/*app.use((request, response, next) => {
-  // custom middleware function
-  console.log('hello from middleware 1');
-  next();
-});
-
+/*
 app.use((request, response, next) => {
   console.log('hello from middleware 2');
   request.requestTime = new Date().toISOString();
@@ -29,5 +24,13 @@ app.use((request, response, next) => {
 // mount the routers, applying the specified middleware routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// if code reaches here, there were no routers to match the request
+app.all('*', (request, response, next) => {
+  response.status(404).json({
+    status: 'fail',
+    message: `can't find ${request.originalUrl}!`
+  });
+});
 
 module.exports = app;
