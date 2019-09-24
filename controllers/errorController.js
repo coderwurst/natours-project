3e3e1701514c70c7fd785db1e8ec214023ec9975
Andrew McMechan <coderwurst@gmail.com>
2019-09-24 19:55:24 +0200

    send more detailed dev errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,8 +2,17 @@ module.exports = (error, request, response, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
-  response.status(error.statusCode).json({
-    status: error.status,
-    message: error.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    response.status(error.statusCode).json({
+      status: error.status,
+      error: error,
+      message: error.message,
+      stack: error.stack
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    response.status(error.statusCode).json({
+      status: error.status,
+      message: error.message
+    });
+  }
 };
