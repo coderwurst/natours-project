bacdb42ca17cfbb1b6af49876d6ce38b0b0a2d2b
Andrew McMechan <coderwurst@gmail.com>
2019-09-24 20:03:30 +0200

    refactor and only send client operational errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,18 +1,37 @@
+const sendErrorDev = (error, response) => {
+  response.status(error.statusCode).json({
+    status: error.status,
+    error: error,
+    message: error.message,
+    stack: error.stack
+  });
+};
+
+const sendErrorProd = (error, response) => {
+  if (error.isOperational) {
+    // trusted error
+    response.status(error.statusCode).json({
+      status: error.status,
+      message: error.message
+    });
+  } else {
+    // programming errors to be logged to console
+    console.error(`ERROR: ${error}`);
+    // then generic message sent to client
+    response.status(500).json({
+      status: 'error',
+      message: 'something went very wrong!'
+    });
+  }
+};
+
 module.exports = (error, request, response, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    response.status(error.statusCode).json({
-      status: error.status,
-      error: error,
-      message: error.message,
-      stack: error.stack
-    });
+    sendErrorDev(error, response);
   } else if (process.env.NODE_ENV === 'production') {
-    response.status(error.statusCode).json({
-      status: error.status,
-      message: error.message
-    });
+    sendErrorProd(error, response);
   }
 };
