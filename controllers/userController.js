00c4098151e8059e5c1ee95b72145b7619539f61
Andrew McMechan <coderwurst@gmail.com>
2019-09-29 13:18:17 +0200

    complete other user crud operations

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,16 +3,19 @@ const catchAsync = require('./../utils/catchAsync');
 
 exports.checkId = (request, response, next, value) => {
   console.log('hello from middleware 3');
-  // TODO: error handling
   next();
 };
 
-exports.getAllUsers = (request, response) => {
-  response.status(500).json({
-    status: 'error',
-    message: 'route not yet defined'
+exports.getAllUsers = catchAsync(async (request, response, next) => {
+  const users = await User.find();
+
+  response.status(200).json({
+    status: 'success',
+    data: {
+      users: users
+    }
   });
-};
+});
 
 exports.getUser = catchAsync(async (request, response, next) => {
   const user = await User.findById(request.params.id);
@@ -25,23 +28,36 @@ exports.getUser = catchAsync(async (request, response, next) => {
   });
 });
 
-exports.createUser = (request, response) => {
-  response.status(500).json({
-    status: 'error',
-    message: 'route not yet defined'
-  });
-};
+exports.createUser = catchAsync(async (request, response) => {
+  const newUser = await User.create(request.body);
 
-exports.updateUser = (request, response) => {
-  response.status(500).json({
-    status: 'error',
-    message: 'route not yet defined'
+  response.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser
+    }
   });
-};
+});
 
-exports.deleteUser = (request, response) => {
-  response.status(500).json({
-    status: 'error',
-    message: 'route not yet defined'
+exports.updateUser = catchAsync(async (request, response) => {
+  const result = await User.findByIdAndUpdate(request.params.id, request.body, {
+    new: true,
+    runValidators: true
   });
-};
+
+  response.status(200).json({
+    status: 'success',
+    data: {
+      user: result
+    }
+  });
+});
+
+exports.deleteUser = catchAsync(async (request, response) => {
+  await User.findByIdAndDelete(request.params.id);
+
+  response.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
