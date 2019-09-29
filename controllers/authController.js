5ad79440763a0bd04baf80013d68263b71783a57
Andrew McMechan <coderwurst@gmail.com>
2019-09-29 13:24:50 +0200

    refactor signup code to prevent injection attacks

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,12 @@ const User = require('../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (request, response, next) => {
-  const newUser = await User.create(request.body);
+  const newUser = await User.create({
+    name: request.body.name,
+    email: request.body.email,
+    password: request.body.password,
+    passwordConfirm: request.body.passwordConfirm
+  });
 
   response.status(201).json({
     status: 'success',
