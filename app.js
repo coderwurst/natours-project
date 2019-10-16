d7c32888047bf1c7ccc70f3a507fb6634a804100
Andrew McMechan <coderwurst@gmail.com>
2019-10-16 21:13:26 +0200

    impl rate limiter to prevent DOS attacks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -9,19 +10,20 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express(); // https://expressjs.com/en/api.html
 
-// middlewares
+// middlewares - GLOBAL SCOPE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // https://github.com/expressjs/morgan/blob/master/index.js
 }
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'request limit exceeded'
+});
+app.use('/api', limiter);
+
 app.use(express.json()); // middleware to add data to request body
 app.use(express.static(`${__dirname}/public`)); // serving static files
-
-/*app.use((request, response, next) => {
-  console.log(request.headers);
-  request.requestTime = new Date().toISOString();
-  next();
-});*/
 
 // mount the routers, applying the specified middleware routers
 app.use('/api/v1/tours', tourRouter);
