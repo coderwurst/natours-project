77f7f2ac66f9469e0814bcdf56eeeb9f68db993a
Andrew McMechan <coderwurst@gmail.com>
2019-10-27 14:34:02 +0100

    pre-find middleware to populate tour and user info

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -37,6 +37,18 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'tour',
+    select: 'name'
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
+
+  next();
+});
+
 const Review = new mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
