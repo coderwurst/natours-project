0ad9a0ca766364856da3a8ea76c11b3567ecb10b
Andrew McMechan <coderwurst@gmail.com>
2019-12-15 19:41:56 +0100

    send mails via sendgrid in production

--- utils/email.js
+++ utils/email.js
@@ -12,8 +12,13 @@ module.exports = class Email {
 
   createNewTransport() {
     if (process.env.NODE_ENV === 'production') {
-      // TODO: sendGrid
-      return 1;
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
+        }
+      });
     }
 
     return nodemailer.createTransport({
